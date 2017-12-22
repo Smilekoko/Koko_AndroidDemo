e4d49df7c3740d3a2f381abe46567608c1333adb
DESKTOP-TP0K5RI\17962 <qq179621228@gmai;.com>
2017-12-22 21:16:14 +0800

    GildeDemo

---
+++ app/src/main/java/com/ak47/www/koko_androiddemo/glide_demo/MyAppGlideModule.java
@@ -0,0 +1,12 @@
+package com.ak47.www.koko_androiddemo.glide_demo;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by 17962 on 2017/12/22.
+ */
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule{
+}
